cb1a409916209feaacc25aaa5ce2422346018503
DS0708 <jonum12312@gmail.com>
2024-11-21 18:30:00 +0900

    first commit

---
+++ README.md
@@ -0,0 +1,1 @@
+# recipe-review-service
---
+++ src/main/java/cloud/zipbob/recipereviewservice/RecipeReviewServiceApplication.java
@@ -0,0 +1,13 @@
+package cloud.zipbob.recipereviewservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RecipeReviewServiceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(RecipeReviewServiceApplication.class, args);
+    }
+
+}
