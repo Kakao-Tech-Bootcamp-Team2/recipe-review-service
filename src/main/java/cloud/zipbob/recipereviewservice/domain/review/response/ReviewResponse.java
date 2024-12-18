21875c633e3f804038c19080f7a3d811f97cde3e
DS0708 <jonum12312@gmail.com>
2024-12-18 09:59:53 +0900

    mongodb 이중화 + 리뷰 생성 response 수정

--- src/main/java/cloud/zipbob/recipereviewservice/domain/review/response/ReviewResponse.java
+++ src/main/java/cloud/zipbob/recipereviewservice/domain/review/response/ReviewResponse.java
@@ -4,14 +4,18 @@ import cloud.zipbob.recipereviewservice.domain.review.Review;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 
+import java.time.LocalDateTime;
+
 @Getter
 @AllArgsConstructor
 public class ReviewResponse {
     private String reviewId;
     private Long memberId;
     private String recipeId;
+    private LocalDateTime createdDate;
+    private LocalDateTime updatedDate;
 
     public static ReviewResponse of(Review review) {
-        return new ReviewResponse(review.getId(), review.getMemberId(), review.getRecipeId());
+        return new ReviewResponse(review.getId(), review.getMemberId(), review.getRecipeId(), review.getCreatedDate(), review.getUpdatedDate());
     }
 }
